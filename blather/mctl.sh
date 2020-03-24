2dd7eefd073bf797816d2d6d29a69f0146e708fe
Lucas Sahar <lucas.sahar@mail.mcgill.ca>
2020-03-24 18:04:42 -0400

    first commit

---
+++ blather/mctl.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+case "$1" in
+    "")
+	mpc play
+	;;
+    
+    pause|toggle|next)
+	mpc "$*"
+	;;
+
+    remove|delete|clear)
+	mpc clear
+	;;
+    
+    skip)
+	mpv next
+	;;
+    back)
+	mpc prev
+	;;
+	
+esac
+
+
+if [ "$1" = play ]; then
+    shift 1
+    args="$(echo "$*" | tr ' ' '*')"
+    if [ -n "$*" ]; then
+	find "$MUSIC" -type f -iname "*$args*" | sed "s|$MUSIC/||" | sort -u | mpc insert &&
+	    (mpc next || mpc play) && echo success
+    else
+	mpc play
+    fi
+    
+fi
+
+if [ "$1" = queue ]; then
+    shift 1
+    args="$(echo "$*" | tr ' ' '*')"
+   [ -n "$*" ] && find "$MUSIC" -type f -iname "*$args*" | sed "s|$MUSIC/||" | sort -u | mpc add &&
+       (mpc next || mpc play) && echo success
+   
+
+fi
+
+
