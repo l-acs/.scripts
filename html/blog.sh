b3b05be92c4c0222a87d555bfa2d849ef5d2ada1
Lucas Sahar <lucas.sahar@mail.mcgill.ca>
2020-04-30 01:46:24 -0400

    functional proof of concept

--- html/blog.sh
+++ html/blog.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+#  To automate blogging
 
-#To automate blogging
 date_readable="$(date +"%A, %b %-d %Y - %-I:%M%P")"
 
 tmp=entry_tmp.html
@@ -11,16 +11,44 @@ footer=something
 template=something
 
 
+#  Goal: try to figure out if this has been run from Emacs. If so,
+#  open in Emacs. Otherwise, open Emacs and edit it there.
 
-
-
-EDIT="emacs -nw" #open in the console
-if [ "$TERM" = 'dumb' ]; then #emacs is running
-    EDIT=unsure #probably emacsclient -c
+EDIT="emacs -nw" # open in the console
+if [ "$TERM" = 'dumb' ]; then # emacs is running
+    EDIT=unsure # probably emacsclient -c
 fi
 
 
+
+cp $template $tmp
+echo "<h4>$date_readable</h4>" >> $tmp
 $EDIT $tmp
+cat $header $tmp $footer > "$filename"
+rm $tmp
 
 
-cat $header $
+echo -n "Please enter the name of your post:  "
+read title
+
+# TODO: add to some monolith of blog entries
+
+
+
+git add $filename
+git commit -m "add entry \'$entry\' via blog.sh"
+git push
+
+
+
+
+
+
+
+
+
+
+
+
+
+
