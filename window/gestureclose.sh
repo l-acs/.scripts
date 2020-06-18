8950b047453fab6c71e397659dbd2245e9a3efb8
l-acs <lucas.sahar@mail.mcgill.ca>
2020-06-18 18:39:28 -0400

    Add support for Signal

--- window/gestureclose.sh
+++ window/gestureclose.sh
@@ -5,13 +5,16 @@ active_win_class="$(xprop -id "$(xdotool getactivewindow)" WM_CLASS | cut -f4 -d
 
 case "$active_win_class" in
     Emacs)
-	key="ctrl+x k"
+	key="ctrl+x k Return"
 	;;
     firefox)
 	key="ctrl+w"
 	;;
     URxvt) # works for tmux, etc; aligns with the intuition that 3 fingers is 'soft' close and 4 'hard'
 	key="ctrl+d"
+	;;
+    Signal) # close media, etc
+	key="Escape"
 	;;
 esac
 
