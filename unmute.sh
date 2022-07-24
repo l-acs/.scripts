5a3fdd1161e3e9e3e0dfb08d50a4fbf8c65bf82f
l-acs <lucas.sahar@mail.mcgill.ca>
2022-07-24 13:56:22 -0400

    Get rid of some junk (not used in years)

--- unmute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-amixer -c 0 sset "Auto-Mute Mode" Disabled
-amixer sset Master unmute
-amixer sset Speaker unmute
-#amixer sset Speaker+LO unmute
-amixer sset Headphone unmute
-
-amixer set Speaker+LO 100%
